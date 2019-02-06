5c2bf85f10bd2b8964252006bd47c2ebd3f00715
Ekaitz Gomez <Ekaitz Gomez@DESKTOP-73K9RLU>
2019-02-06 11:25:31 +0100

    no tocar

---
+++ src/modelo/dao/UsuarioModelo.java
@@ -0,0 +1,41 @@
+package modelo.dao;
+
+import java.util.ArrayList;
+
+import modelo.bean.Usuario;
+
+public class UsuarioModelo {
+
+	// ArrayList<Usuario> selectTodos = new ArrayList<Usuario>();
+
+	/**
+	 * seleccionar todos los usuarios y devolver un ArrayList
+	 * 
+	 * @return ArrayList<Usuario>
+	 */
+	public ArrayList<Usuario> selectTodos() {
+		return null; 
+	}
+
+	/**
+	 * añadir usuarios
+	 * 
+	 * @return boolean si se ha añadido o no
+	 */
+
+	public boolean insert(Usuario usuario) {
+
+		return false;
+
+	}
+
+	/**
+	 * borrar un usuario
+	 * 
+	 * @return si se ha añadido o no
+	 */
+	public boolean delete(Usuario usuario) {
+
+		return false;
+	}
+}
